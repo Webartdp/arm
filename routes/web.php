3264945a0af97eef99e9f9a1f143f685ffc4c7ca
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-11 14:06:11 +0300

    Add public verified archive download route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Front\ContentPageController;
+use App\Http\Controllers\Front\DocumentDownloadController;
 use App\Http\Controllers\Front\HomeController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ Route::get('/{locale}/about', [ContentPageController::class, 'about'])
 Route::get('/{locale}/statistics', [ContentPageController::class, 'statistics'])
     ->whereIn('locale', ['am', 'en', 'ru'])
     ->name('front.statistics');
+
+Route::get('/{locale}/document/{trackingNumber}/download', DocumentDownloadController::class)
+    ->whereIn('locale', ['am', 'en', 'ru'])
+    ->where('trackingNumber', '[A-Za-z0-9-]+')
+    ->middleware('throttle:30,1')
+    ->name('front.document.download');
 
 Route::get('/{locale}', HomeController::class)
     ->whereIn('locale', ['am', 'en', 'ru'])
