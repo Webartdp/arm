9b6e32a8701f2edf6133b1ddf253ec70041d9820
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-11 07:57:36 +0300

    Cast editable page lists to arrays

--- app/Models/SiteSetting.php
+++ app/Models/SiteSetting.php
@@ -7,4 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class SiteSetting extends Model
 {
     protected $guarded = [];
+
+    protected function casts(): array
+    {
+        return [
+            'about_items_ru' => 'array',
+            'about_items_en' => 'array',
+            'about_items_am' => 'array',
+            'statistics_items_ru' => 'array',
+            'statistics_items_en' => 'array',
+            'statistics_items_am' => 'array',
+        ];
+    }
 }
