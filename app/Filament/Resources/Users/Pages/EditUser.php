07a86e97c0a0f102e65cd0f1fc19005f0486522b
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-11 07:09:56 +0300

    Keep password blank when editing users

--- app/Filament/Resources/Users/Pages/EditUser.php
+++ app/Filament/Resources/Users/Pages/EditUser.php
@@ -8,4 +8,11 @@ use Filament\Resources\Pages\EditRecord;
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
+
+    protected function mutateFormDataBeforeFill(array $data): array
+    {
+        $data['password'] = null;
+
+        return $data;
+    }
 }
