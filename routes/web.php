aa1a9a636d319cf28e2c4228d3aac9403b9ed530
Webartdp <37107616+Webartdp@users.noreply.github.com>
2026-08-11 07:59:44 +0300

    Add localized About and Statistics routes

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use App\Http\Controllers\Front\ContentPageController;
 use App\Http\Controllers\Front\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::redirect('/', '/en');
+
+Route::get('/{locale}/about', [ContentPageController::class, 'about'])
+    ->whereIn('locale', ['am', 'en', 'ru'])
+    ->name('front.about');
+
+Route::get('/{locale}/statistics', [ContentPageController::class, 'statistics'])
+    ->whereIn('locale', ['am', 'en', 'ru'])
+    ->name('front.statistics');
 
 Route::get('/{locale}', HomeController::class)
     ->whereIn('locale', ['am', 'en', 'ru'])
